ddcd9c41e502b9e46b95f9a9433b8e8448cbb1dd
Varun-666 <varunfatehpuria2@gmail.com>
2024-07-27 20:27:51 +0530

    Updated aand made he user detail working

--- oops/userDetail.py
+++ oops/userDetail.py
@@ -1,35 +1,55 @@
 class Student:
-        def __init__(self, name:str, age:int, score:int, id:int):
-            self.name = name
-            self.age = age
-            self.score = score
-            self.id = id
-        def __str__(self):
-              return f"ID: {self.id} | Name: {self.name} | Age: {self.age} | Score: {self.score}"
-        
-running = True
+    def __init__(self, name: str, age: int, score: int, id: int):
+        self.name = name
+        self.age = age
+        self.score = score
+        self.id = id
+
+    def __str__(self):
+        return f"ID: {self.id} | Name: {self.name} | Age: {self.age} | Score: {self.score}"
+
+class DuplicateError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+def get_int(prompt: str) -> int:
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+def check_dup_id(id: int, students: list) -> bool:
+    for std in students:
+        if std.id == id:
+            return True
+    return False
 
 students = []
+running = True
 
-def checkDupID(id):
-      found = False
-      for std in students:
-            found = (std.in == std and found = False)
-      if(found):
-            print("ID alreaddy exists! Please provide another ID")
-            return checkDupID("ID: ")
-      else:
-            return id
-      
-while(running):
-      n = input("Enter Name: ")
-      if(n.lower()=='quit'):
-            for std in students:
-                  print(std)
-            print("||||Thank You||||")
-      else:
-            a = getInt("Enter Age: ")
-            s = getInt("Enter Score: ")
-            students.append(
-                Student(n, a, s)
-            )     
+while running:
+    n = input("Enter Name: ")
+    if n.lower() == 'quit':
+        running = False
+    else:
+        a = get_int("Enter Age: ")
+        s = get_int("Enter Score: ")
+
+        while True:
+            id = get_int("Enter ID: ")
+            if not check_dup_id(id, students):
+                students.append(Student(n, a, s, id))
+                break
+            else:
+                print("ID already exists! Please enter another ID.")
+
+# Print all student information
+print("|||| Students Information ||||")
+for std in students:
+    print(std)
+print("|||| Thank You ||||")
