dc7500202c92a1f108c57451c4a3dac945978fbd
Varun-666 <varunfatehpuria20@gmail.com>
2024-08-10 14:02:05 +0530

    Added DoublyLinkedList

---
+++ oops/double.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begining(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        new_node.prev = last_node
+        
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end = "<-->")
+            current_node = current_node.next
+        print("None")
