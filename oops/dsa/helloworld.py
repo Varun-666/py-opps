b7a330df26bac95deaab0211cc39abaa4af710aa
Varun-666 <varunfatehpuria20@gmail.com>
2024-08-10 17:26:33 +0530

    Updated File & Added A New File

---
+++ oops/dsa/helloworld.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number to check wheater it is smaller or larger than 10: "))
+if(n > 10):
+    print("The entered number is larger than 10")
+else:
+    print("The entered number is smaller than 10")
